592fcee91d4a321b373941619fc8cf9b04970b6b
adamejhonrey <95111969+adamejhonrey@users.noreply.github.com>
2022-12-18 23:20:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Query the bot for an answer
 async function queryBot(message) {
+  // Add a loading indicator to the message history
+  addLoadingIndicator();
   // Make a request to the bot API
   const response = await fetch('/api/bot', {
     method: 'POST',
@@ -8,39 +10,28 @@ async function queryBot(message) {
     },
     body: JSON.stringify({ message })
   });
+  // Remove the loading indicator from the message history
+  removeLoadingIndicator();
   // Return the response from the bot
   return await response.json();
 }
 
-// Send a message to the bot
-async function sendMessage() {
-  // Get the message input element and value
-  const inputEl = document.querySelector('#message-input');
-  const message = inputEl.value;
-  // Clear the input field
-  inputEl.value = '';
-  // Query the bot for an answer
-  const answer = await queryBot(message);
-  // Add the message to the message history
-  addMessage(message, 'user');
-  // Add the bot's answer to the message history
-  addMessage(answer.text, 'bot');
+function addLoadingIndicator() {
+  // Clone the template loading element
+  const loadingEl = document.querySelector('#template-loading').cloneNode(true);
+  // Remove the "id" attribute from the loading element
+  loadingEl.removeAttribute('id');
+  // Add the loading element to the message history
+  document.querySelector('#message-history').appendChild(loadingEl);
 }
 
-// Add a message to the message history
-function addMessage(text, sender) {
-  // Clone the template message element
-  const messageEl = document.querySelector('#template-message').cloneNode(true);
-  // Set the message text
-  messageEl.querySelector('.message-content').textContent = text;
-  // Set the message sender
-  messageEl.querySelector('.message-sender').textContent = sender;
-  // Add the message element to the message history
-  document.querySelector('#message-history').appendChild(messageEl);
+// Remove the loading indicator from the message history
+function removeLoadingIndicator() {
+  // Get the last message in the message history
+  const lastMessageEl = document.querySelector('#message-history .message:last-child');
+  // Check if the last message is a loading indicator
+  if (lastMessageEl.classList.contains('loading')) {
+    // Remove the loading indicator from the message history
+    lastMessageEl.remove();
+  }
 }
-
-// Handle form submission
-document.querySelector('#message-form').addEventListener('submit', e => {
-  e.preventDefault();
-  sendMessage();
-});
